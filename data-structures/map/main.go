ea9a63a5d617e421b551b69e561834a35513a558
Satyendra Singh <satyendra.singh3339@gmail.com>
2022-10-06 23:50:11 -0700

    Update main.go

--- data-structures/map/main.go
+++ data-structures/map/main.go
@@ -5,21 +5,9 @@ import "fmt"
 // NewSubscriptionsMap function to create sunscription map
 func NewSubscriptionsMap() map[string]bool {
 	return map[string]bool{
-		"monitoring":     false,
-		"apprf":          false,
-		"presence":       false,
-		"cloudguest":     false,
-		"usermanagement": false,
-		"visualrf":       false,
-		"contextengine":  false,
-		"ucc":            false,
-		"lync":           false,
-		"audit":          false,
-		"clickstream":    false,
-		"location":       false,
-		"rapids":         false,
-		"syslog":         false,
-		"stream-stats":   false,
+		"test":     false,
+		"test1":          false,
+		"test2":       false,
 	}
 }
 
